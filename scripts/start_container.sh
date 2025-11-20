b133a936f9f9bc9de2917635c073e05aa9512a38
sharmasudhir1234 <37590384+sharmasudhir1234@users.noreply.github.com>
2025-11-20 14:52:02 +0530

    changing docker port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,6 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull sudhirsharma/simple-python-flask-app
 
+
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sudhirsharma/simple-python-flask-app
+docker run -d -p 8080:5000 sudhirsharma/simple-python-flask-app
